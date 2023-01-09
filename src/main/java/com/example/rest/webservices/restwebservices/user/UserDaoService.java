9a74896a04aacfedecf91e4dcc1fb9762515af34
ARUNDHATI <arundhati@ARUNDHATIs-MacBook-Pro.local>
2023-01-09 14:55:10 +0530

    changes

--- src/main/java/com/example/rest/webservices/restwebservices/user/UserDaoService.java
+++ src/main/java/com/example/rest/webservices/restwebservices/user/UserDaoService.java
@@ -12,13 +12,15 @@ public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
 	
+	private static int userCount=0;
+	
 	static
 	
 	{
-		users.add(new User (1 , "ABC" , LocalDate.now().minusYears(30)));
-		users.add(new User (2 , "CDF" , LocalDate.now().minusYears(25)));
-		users.add(new User (3 , "FGH" , LocalDate.now().minusYears(20)));
-		users.add(new User (4 , "IJK" , LocalDate.now().minusYears(15)));
+		users.add(new User (userCount++ , "ABC" , LocalDate.now().minusYears(30)));
+		users.add(new User (userCount++ , "CDF" , LocalDate.now().minusYears(25)));
+		users.add(new User (userCount++, "FGH" , LocalDate.now().minusYears(20)));
+		users.add(new User (userCount++ , "IJK" , LocalDate.now().minusYears(15)));
 	}
 
 	public List<User>  findAll()
@@ -34,4 +36,15 @@ public class UserDaoService {
 		return users.stream().filter(predicate).findFirst().get();
 				
 	}
+
+	public User save(User user) {
+		// TODO Auto-generated method stub
+		user.setId(userCount++);
+		users.add(user);
+		
+		return user;
+		
+	}
+	
+	
 }
--- src/main/java/com/example/rest/webservices/restwebservices/user/UserReource.java
+++ src/main/java/com/example/rest/webservices/restwebservices/user/UserReource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,4 +34,10 @@ public class UserReource {
 		
 	}
 
+	@PostMapping("/users")
+	public  void createUser(@RequestBody User user)
+	{
+		service.save(user);
+		
+	}
 }
